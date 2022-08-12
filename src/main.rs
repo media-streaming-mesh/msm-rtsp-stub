a22f71181eaf96b596071273122b3f126d960030
Giles Heron <giheron@cisco.com>
2022-08-12 17:40:07 +0100

    change from command line parms to env vars

--- src/main.rs
+++ src/main.rs
@@ -20,90 +20,64 @@ use msm_rtsp_stub::dp::dp_init;
 
 use futures::future::join_all;
 use http::Uri;
-use log::{trace, debug, info, error, Level};
-use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use log::{trace, info, error};
+use std::net::{IpAddr, SocketAddr};
+use std::str::FromStr;
 
 use simple_logger;
-
-use clap::Parser;
-
-/// Simple program to greet a person
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-        /// Log level
-        #[clap(long)]
-        level: Option<Level>,
-
-        /// Listening port
-        #[clap(long)]
-        port: Option<u16>,
-
-        /// CP URI
-        #[clap(long)]
-        control_plane: Option<http::uri::Uri>,
-
-        /// DP socket
-        #[clap(long)]
-        data_plane: Option<std::net::SocketAddr>,
-}
+use envmnt;
 
 #[tokio::main (flavor="current_thread")]
 async fn main() {
 
-    let args = Args::parse();
-    let mut level = Level::Error;
-    let mut port: u16 = 8554;
-    let mut control_plane: Uri = "http://127.0.0.1:9000".parse().unwrap();
-    let mut data_plane = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8050);
+    // We prefer to use LOG_LVL rather than RUST_LOG
+    envmnt::set("RUST_LOG", envmnt::get_or("LOG_LVL", "WARN"));
 
-    match args.level {
-        Some(log_level) => level = log_level,
-        None => {},
-    }
-
-    // seemed like a reasonable choice at the time
-    match simple_logger::init_with_level(level) {
+    match simple_logger::init_with_env() {
         Ok(()) => {
-            match args.port {
-                Some(listen_port) => port = listen_port,
-                None => debug!("no valid port parameter given - using default"),
-            }
-        
-            match args.control_plane {
-                Some(cp) => control_plane = cp,
-                None => debug!("no valid control_plane parameter given - using default"),
-            }
-        
-            match args.data_plane {
-                Some(dp) => data_plane = dp,
-                None => debug!("no valid data_plane parameter given - using default"),
-            }
-        
-            let mut handles = vec![];
-            // spawn a green thread for the client communication
-            handles.push(tokio::spawn(async move {
-                match client_listener(format!(":::{}", port).to_string()).await {
-                    Ok(()) => info!("Disconnected!"),
-                    Err(e) => error!("Error: {}", e),
+            let rtp_port = envmnt::get_u16("RTP_PROXY_PORT", 8050);
+            let rtsp_port = envmnt::get_u16("RTSP_PROXY_PORT", 8554);
+            
+            match IpAddr::from_str(&envmnt::get_or("RTP_DATA_PLANE_IP", "127.0.0.1")) {
+                Ok(data_plane_ip) => {
+                    let data_plane = SocketAddr::new(data_plane_ip, rtp_port);
+
+                    match Uri::from_str(&envmnt::get_or("RTP_CONTROL_PLANE", "http://127.0.0.1:9000")) {
+                        Ok(control_plane) => {
+                            let mut handles = vec![];
+                            // spawn a green thread for the client communication
+                            handles.push(tokio::spawn(async move {
+                                match client_listener(format!(":::{}", rtsp_port).to_string()).await {
+                                    Ok(()) => info!("Disconnected!"),
+                                    Err(e) => error!("Error: {}", e),
+                                }
+                            }));
+                        
+                            // spawn a green thread for the CP communication
+                            handles.push(tokio::spawn(async move {
+                                match cp_connector(control_plane).await {
+                                    Ok(()) => info!("Disconnected!"),
+                                    Err(e) => error!("Error: {}", e),
+                                }
+                            }));
+                        
+                            match dp_init(data_plane).await {
+                                Ok(()) => trace!("Connected to DP"),
+                                Err(e) => error!("Error connecting to DP: {}", e),
+                            }
+                        
+                            // wait for all green threads to finish (not gonna happen)
+                            join_all(handles).await;
+                        },
+                        Err(e) => {
+                            error!("unable to parse control plane URI {}", e.to_string());
+                        }
+                    }
+                },
+                Err(e) => {
+                    error!("unable to parse data plane IP address {}", e.to_string());
                 }
-            }));
-        
-            // spawn a green thread for the CP communication
-            handles.push(tokio::spawn(async move {
-                match cp_connector(control_plane).await {
-                    Ok(()) => info!("Disconnected!"),
-                    Err(e) => error!("Error: {}", e),
-                }
-            }));
-        
-            match dp_init(data_plane).await {
-                Ok(()) => trace!("Connected to DP"),
-                Err(e) => error!("Error connecting to DP: {}", e),
             }
-        
-            // wait for all green threads to finish (not gonna happen)
-            join_all(handles).await;
         },
         Err(e) => {
             error!("unable to log: {}", e.to_string());
