3b231ed37d06846dfd91f28ce13e648425e03235
Sri Aradhyula <sraradhy@cisco.com>
2022-12-12 22:36:48 -0600

    chore: use msm-rtsp-stub

---
+++ .jenkins/build-docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+# Syntax build-docker.sh [-i|--image imagename]
+
+PROJECT=msm-rtsp-stub
+DOCKER_IMAGE=${PROJECT}:latest
+
+while [[ $# -gt 0 ]]
+do
+    key="${1}"
+
+    case ${key} in
+    -i|--image)
+        DOCKER_IMAGE="${2}"
+        shift;shift
+        ;;
+    -h|--help)
+        less README.md
+        exit 0
+        ;;
+    *) # unknown
+        echo Unknown Parameter $1
+        exit 4
+    esac
+done
+
+echo BUILDING DOCKER ${DOCKER_IMAGE}
+docker build --no-cache -t ${DOCKER_IMAGE} -f Dockerfile .
