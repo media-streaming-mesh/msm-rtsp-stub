dd1fef01c6910be2bf18a3c79e73cae34cb5a399
ahismail <ahismail@cisco.com>
2023-01-12 16:38:02 -0800

    fix: install docker buildx cli plugin

--- .jenkins/build-docker.sh
+++ .jenkins/build-docker.sh
@@ -23,6 +23,10 @@ do
     esac
 done
 echo BUILDING DOCKER ${DOCKER_IMAGE}
-docker buildx install
+export DOCKER_BUILDKIT=1
+docker build --platform=local -o . git://github.com/docker/buildx
+mkdir -p ~/.docker/cli-plugins
+mv buildx ~/.docker/cli-plugins/docker-buildx
+docker buildx ls
 docker buildx create --name=multi-arch-images --driver=docker-container --use
 docker buildx build --platform linux/amd64,linux/arm64 -t ${DOCKER_IMAGE} -f  ../Dockerfile .
