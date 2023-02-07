da78c7bfb09124b18ac55af3e8d3e5cb6eb6dd45
ahismail <ahismail@cisco.com>
2023-02-06 16:49:04 -0800

    debug#4

--- .jenkins/build-docker.sh
+++ .jenkins/build-docker.sh
@@ -22,18 +22,8 @@ do
     esac
 done
 echo BUILDING DOCKER ${DOCKER_IMAGE}
-# enable experimental buildx features
 docker version
-rm -rf ~/.docker/cli-plugins/
-mkdir -p ~/.docker/cli-plugins/
-BUILDX_LATEST_BIN_URI=$(curl -s -L https://github.com/docker/buildx/releases/latest | grep 'linux-amd64' | grep 'href' | sed 's/.*href="/https:\/\/github.com/g; s/amd64".*/amd64/g')
-curl -s -L ${BUILDX_LATEST_BIN_URI} -o ~/.docker/cli-plugins/docker-buildx
-chmod a+x ~/.docker/cli-plugins/docker-buildx
-# Get and run the latest docker/binfmt tag to use its qemu parts
-BINFMT_IMAGE_TAG=$(curl -s https://registry.hub.docker.com/v2/repositories/docker/binfmt/tags | jq '.results | sort_by(.last_updated)[-1].name' -r)
-docker run --rm --privileged docker/binfmt:${BINFMT_IMAGE_TAG}
 # create a build instance
-# docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
 docker buildx rm  ${ARCH} | true
 docker buildx create --name=${ARCH} --driver=docker-container --use 
 docker buildx build --platform linux/amd64,linux/arm64 -t ${DOCKER_IMAGE} -f Dockerfile .
